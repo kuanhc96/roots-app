3987cf0a5fd5c7ec3b7fffec901e602c53eca1e3
Kuan <chenkuansk@ad.garmin.com>
2026-04-21 17:34:12 +0800

    feat: create landing (home) page

---
+++ web-client/app/pages/home.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container class="px-6">
+    <v-row justify="center" class="mb-4">
+      <v-col cols="10" md="4">
+        <HomeCard title="Announcements" />
+      </v-col>
+      <v-col cols="10" md="4">
+        <HomeCard title="Small Group" />
+      </v-col>
+    </v-row>
+    <v-row justify="center" class="mb-4">
+      <v-col cols="10" md="4">
+        <HomeCard title="Service Areas" />
+      </v-col>
+      <v-col cols="10" md="4">
+        <HomeCard title="My Profile" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
