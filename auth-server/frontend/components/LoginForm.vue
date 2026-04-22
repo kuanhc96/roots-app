9e3e8076c8f8f748a3e967509314f9db7fba1438
Kuan <chenkuansk@ad.garmin.com>
2026-04-22 16:47:50 +0800

    feat: replace placeholder landing page with login form

---
+++ auth-server/frontend/components/LoginForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card width="400">
+    <v-card-title>Login</v-card-title>
+    <v-card-text>
+      <v-text-field v-model="email" label="Email" type="email" />
+      <v-text-field v-model="password" label="Password" type="password" />
+    </v-card-text>
+    <v-card-actions>
+      <v-btn @click="login">Login</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+const email = ref('')
+const password = ref('')
+function login() {}
+</script>
