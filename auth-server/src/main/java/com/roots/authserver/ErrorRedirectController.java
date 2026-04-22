ff891c168995f4b2f1514f87bee795fd7f1d5f51
Kuan <chenkuansk@ad.garmin.com>
2026-04-22 17:29:04 +0800

    feat: handle whitelabel errors

---
+++ auth-server/src/main/java/com/roots/authserver/ErrorRedirectController.java
@@ -0,0 +1,14 @@
+package com.roots.authserver;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorRedirectController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        return "redirect:/login";
+    }
+}
