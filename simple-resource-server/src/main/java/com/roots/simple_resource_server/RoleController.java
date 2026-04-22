e4cd9f1e008dab2bbe851212f9325895ae2508bd
Kuan <chenkuansk@ad.garmin.com>
2026-04-22 14:27:41 +0800

    feat: add CORS protection to RoleController

--- simple-resource-server/src/main/java/com/roots/simple_resource_server/RoleController.java
+++ simple-resource-server/src/main/java/com/roots/simple_resource_server/RoleController.java
@@ -1,9 +1,11 @@
 package com.roots.simple_resource_server;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "${web.client.origin:http://localhost:3000}")
 @RestController
 @RequestMapping("/api/role")
 public class RoleController {
