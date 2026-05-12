2d9cd429705bc56549661d93b590603094af5dfd
Kuan <chenkuansk@ad.garmin.com>
2026-05-12 08:52:51 +0800

    feat: upgrade username/password authentication provider for mfa

--- auth-server/src/main/java/com/roots/authserver/component/MfaAwareDaoAuthenticationProvider.java
+++ auth-server/src/main/java/com/roots/authserver/component/MfaAwareDaoAuthenticationProvider.java
@@ -14,7 +14,7 @@ import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
-public class MfaAwareAuthenticationProvider implements AuthenticationProvider {
+public class MfaAwareDaoAuthenticationProvider implements AuthenticationProvider {
     private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
 
