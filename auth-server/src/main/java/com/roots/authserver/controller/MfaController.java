94dd68431a5d34bf321c24cef66d255298c79f8f
Kuan <chenkuansk@ad.garmin.com>
2026-05-12 08:56:14 +0800

    feat: generate OTT

--- auth-server/src/main/java/com/roots/authserver/controller/MfaController.java
+++ auth-server/src/main/java/com/roots/authserver/controller/MfaController.java
@@ -1,4 +1,27 @@
 package com.roots.authserver.controller;
 
-public class RestController {
+import java.security.Principal;
+
+import org.springframework.security.authentication.ott.GenerateOneTimeTokenRequest;
+import org.springframework.security.authentication.ott.InMemoryOneTimeTokenService;
+import org.springframework.security.authentication.ott.OneTimeToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class MfaController {
+    private final InMemoryOneTimeTokenService oneTimeTokenService;
+
+    @PostMapping("/ott/generate")
+    public void generateOneTimeToken() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        GenerateOneTimeTokenRequest generateOneTimeTokenRequest = new GenerateOneTimeTokenRequest(authentication.getName());
+        OneTimeToken oneTimeToken = oneTimeTokenService.generate(generateOneTimeTokenRequest);
+        System.out.println(oneTimeToken.getTokenValue());
+    }
 }
