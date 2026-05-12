1ed196e6b7413ba798d717623391c90d53d93673
Kuan <chenkuansk@ad.garmin.com>
2026-05-12 08:51:33 +0800

    feat: add mfa pending and mfa authentication tokens

--- auth-server/src/main/java/com/roots/authserver/principal/MfaAuthenticationToken.java
+++ auth-server/src/main/java/com/roots/authserver/principal/MfaAuthenticationToken.java
@@ -1,4 +1,13 @@
 package com.roots.authserver.principal;
 
-public class MfaAuthenticationToken {
+import java.util.Collection;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class MfaAuthenticationToken extends UsernamePasswordAuthenticationToken {
+    public MfaAuthenticationToken(UserDetails principal, Collection<? extends GrantedAuthority> authorities) {
+        super(principal, "", authorities);
+    }
 }
--- auth-server/src/main/java/com/roots/authserver/principal/MfaPendingAuthenticationToken.java
+++ auth-server/src/main/java/com/roots/authserver/principal/MfaPendingAuthenticationToken.java
@@ -1,4 +1,19 @@
 package com.roots.authserver.principal;
 
-public class MfaPendingAuthenticationToken {
+import java.util.Collections;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class MfaPendingAuthenticationToken extends AbstractAuthenticationToken {
+    private final UserDetails principal;
+
+    public MfaPendingAuthenticationToken(UserDetails principal) {
+        super(Collections.emptyList());
+        this.principal = principal;
+        setAuthenticated(false);
+    }
+
+    @Override public Object getCredentials() {return "";}
+    @Override public Object getPrincipal() {return principal;}
 }
