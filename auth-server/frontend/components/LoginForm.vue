3c42628982112897695974812d2b5f238d36d340
Kuan <chenkuansk@ad.garmin.com>
2026-05-15 16:28:55 +0800

    feat: move login as guest button

--- auth-server/frontend/components/LoginForm.vue
+++ auth-server/frontend/components/LoginForm.vue
@@ -1,26 +1,21 @@
 <template>
   <div>
-    <v-card width="400">
-      <form method="post" action="/login">
-        <v-card-title>Login</v-card-title>
-        <v-card-text>
-          <v-text-field name="email" label="Email" type="email" />
-          <v-text-field name="password" label="Password" type="password" />
-          <v-checkbox name="remember-me" value="true" label="Remember Me?" />
-        </v-card-text>
-        <v-card-actions>
-          <v-btn type="submit">Login</v-btn>
-        </v-card-actions>
-      </form>
-    </v-card>
+    <form id="login-form" method="post" action="/login"></form>
+    <form id="guest-form" method="post" action="/login/guest"></form>
 
-    <form method="post" action="/login/guest">
-      <v-card width="400" class="mt-2">
-        <v-card-actions class="justify-center">
-          <v-btn type="submit" variant="text">Continue as Guest</v-btn>
-        </v-card-actions>
-      </v-card>
-    </form>
+    <v-card width="400">
+      <v-card-title>Login</v-card-title>
+      <v-card-text>
+        <v-text-field name="email" label="Email" type="email" form="login-form" />
+        <v-text-field name="password" label="Password" type="password" form="login-form" />
+        <v-checkbox name="remember-me" value="true" label="Remember Me?" form="login-form" />
+      </v-card-text>
+      <v-card-actions>
+        <v-btn type="submit" form="login-form">Login</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn type="submit" form="guest-form" variant="text">Continue as Guest</v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
