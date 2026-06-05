fffec80d0f1bef8b030f65af8726c8e9ec9ca9d1
Kuan <chenkuansk@ad.garmin.com>
2026-06-05 11:07:39 +0800

    feat: send out magic link token when CreateAccountPendingAuthenticationToken detected

--- auth-server/src/main/java/com/roots/authserver/component/MfaRedirectAuthenticationSuccessHandler.java
+++ auth-server/src/main/java/com/roots/authserver/component/MfaRedirectAuthenticationSuccessHandler.java
@@ -2,22 +2,55 @@ package com.roots.authserver.component;
 
 import java.io.IOException;
 
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.authentication.ott.GenerateOneTimeTokenRequest;
+import org.springframework.security.authentication.ott.InMemoryOneTimeTokenService;
+import org.springframework.security.authentication.ott.OneTimeToken;
+import org.springframework.security.authentication.ott.OneTimeTokenService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.roots.authserver.principal.CreateAccountPendingAuthenticationToken;
 import com.roots.authserver.principal.MfaPendingAuthenticationToken;
+import com.roots.authserver.service.EmailService;
+import com.roots.authserver.service.InMemoryOneTimePinService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class MfaRedirectAuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
 
+    private final InMemoryOneTimePinService inMemoryOneTimePinService;
+    private final InMemoryOneTimeTokenService inMemoryOneTimeTokenService;
+    private final EmailService emailService;
+
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication auth) throws IOException, ServletException {
+    public void onAuthenticationSuccess(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull Authentication auth
+    ) throws IOException, ServletException {
         if (auth instanceof MfaPendingAuthenticationToken) {
+            GenerateOneTimeTokenRequest generateOneTimeTokenRequest = new GenerateOneTimeTokenRequest(auth.getName());
+            OneTimeToken oneTimeToken = inMemoryOneTimePinService.generate(generateOneTimeTokenRequest);
+            emailService.sendOTTEmail(auth.getName(), oneTimeToken.getTokenValue());
             response.sendRedirect("/ott/login");
+        } else if (auth instanceof CreateAccountPendingAuthenticationToken) {
+            GenerateOneTimeTokenRequest generateOneTimeTokenRequest = new GenerateOneTimeTokenRequest(auth.getName());
+            OneTimeToken oneTimeToken = inMemoryOneTimeTokenService.generate(generateOneTimeTokenRequest);
+            String magicLink = ServletUriComponentsBuilder.fromCurrentContextPath()
+                    .path("/magic-link/login")
+                    .queryParam("magicLinkToken", oneTimeToken.getTokenValue())
+                    .build()
+                    .toUriString();
+            System.out.println("MAGIC LINK: " + magicLink);
+            emailService.sendMagicLinkEmail(auth.getName(), magicLink);
+            response.sendRedirect("/signup/success");
         } else {
             super.onAuthenticationSuccess(request, response, auth);
         }
