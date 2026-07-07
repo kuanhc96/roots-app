1e0f98405a9b22ac11374c83947712717df821d2
Kuan <chenkuansk@ad.garmin.com>
2026-07-07 16:49:03 +0800

    fix: OTP was being generated twice

--- auth-server/frontend/pages/ott/login.vue
+++ auth-server/frontend/pages/ott/login.vue
@@ -1,20 +1,4 @@
 <script setup lang="ts">
-onBeforeMount(() => {
-  fetch('/ott/generate', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  }).then(response => {
-    if (response.status === 403) {
-      useRouter().replace('/login?e=no_mfa_pending')
-    } else if (!response.ok) {
-      console.log("Failed to generate OTT token")
-    } else {
-      console.log("generated OTT token successfully")
-    }
-  })
-})
 </script>
 
 <template>
