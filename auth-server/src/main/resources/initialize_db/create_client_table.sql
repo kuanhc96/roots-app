b40fe1bca1d6a42ae1e089fb603497f894da774d
Kuan <chenkuansk@ad.garmin.com>
2026-09-03 16:55:53 +0800

    feat: super client should include same access as basic one

--- auth-server/src/main/resources/initialize_db/create_client_table.sql
+++ auth-server/src/main/resources/initialize_db/create_client_table.sql
@@ -174,7 +174,7 @@ INSERT INTO oauth2_registered_client (
              'authorization_code',
              'http://localhost:8084/api/auth/callback',
              'http://localhost:3001/logout',
-             'openid,ACCOUNT_MANAGEMENT_SUPER_CLIENT',
+             'openid,ACCOUNT_MANAGEMENT_CLIENT,ACCOUNT_MANAGEMENT_SUPER_CLIENT',
              '{"@class":"java.util.Collections$UnmodifiableMap","settings.client.require-proof-key":true,"settings.client.require-authorization-consent":false}',
              '{"@class":"java.util.Collections$UnmodifiableMap","settings.token.reuse-refresh-tokens":false,"settings.token.x509-certificate-bound-access-tokens":false,"settings.token.id-token-signature-algorithm":["org.springframework.security.oauth2.jose.jws.SignatureAlgorithm","RS256"],"settings.token.access-token-time-to-live":["java.time.Duration",300.000000000],"settings.token.access-token-format":{"@class":"org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat","value":"self-contained"},"settings.token.refresh-token-time-to-live":["java.time.Duration",3600.000000000],"settings.token.authorization-code-time-to-live":["java.time.Duration",300.000000000],"settings.token.device-code-time-to-live":["java.time.Duration",300.000000000]}'
          );
