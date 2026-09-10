bd68871fab85845f5484c75b8e57d177f4cdbe0f
Kuan <chenkuansk@ad.garmin.com>
2026-09-10 16:47:49 +0800

    initialize account-management-client-bff

---
+++ account-management-client-bff/src/main/java/com/roots/account_management_client_bff/AccountManagementClientBffApplication.java
@@ -0,0 +1,13 @@
+package com.roots.account_management_client_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManagementClientBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManagementClientBffApplication.class, args);
+	}
+
+}
---
+++ account-management-client-bff/src/test/java/com/roots/account_management_client_bff/AccountManagementClientBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roots.account_management_client_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagementClientBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
